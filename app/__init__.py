cd101c5cea1376bc1a57b2b63a951007aa6c5ee8
backgroundclouds <142837972+backgroundclouds@users.noreply.github.com>
2025-02-17 17:11:14 -0500

    compression removed

--- app/__init__.py
+++ app/__init__.py
@@ -10,11 +10,6 @@ def create_app():
     # Enable caching for static files
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 3600  # Cache for 1 hour
 
-    # Enable compression if possible
-    app.config['COMPRESS_MIMETYPES'] = ['text/html', 'text/css', 'application/json', 'application/javascript', 'image/png', 'image/jpeg', 'video/mp4']
-    app.config['COMPRESS_LEVEL'] = 6
-    
-
     # Load data from YAML file
     with open('data/projects.yaml', 'r') as file:
         projects = yaml.safe_load(file)
@@ -35,10 +30,3 @@ if __name__ == '__main__':
     app = create_app()
     port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port)
-
-
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
